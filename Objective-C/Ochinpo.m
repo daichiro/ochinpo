3de12fccd8d0eb4a37b45fc63e4506a3837aecef
mokumoku <da1_1hara@me.com>
2013-11-22 01:49:59 +0900

    NSStringのAPIを利用

--- Objective-C/Ochinpo.m
+++ Objective-C/Ochinpo.m
@@ -10,12 +10,12 @@
     int rnd = 0;
     int complete = 0;
     int cnt = 0;
-    NSString *ocnp[] = {@"お", @"ち", @"ん", @"ぽ"};
+    NSString *ocnp = @"おちんぽ";
 
     while (complete < 4) {
         cnt++;
         rnd = rand()%4;
-        printf("%s", [ocnp[rnd] UTF8String]);
+        printf("%s", [[ocnp substringWithRange:NSMakeRange(rnd,1)] UTF8String]);
         complete = (complete == rnd) ? ++complete : 0;
     }
 
